6540d6eb9126a7fab89ee6edab311f7de3998af2
sho endo <endo.sho@moneyforward.co.jp>
2020-04-24 11:08:40 +0900

    Display login status

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import axios from 'axios';
 
 import Home from './Home';
 import About from './About';
 import Login from './Login';
+import SignUp from './Signup';
 
 const App: FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_HOST}/logged_in`, { withCredentials: true })
+      .then(res => {
+        setIsLoggedIn(res.data.logged_in);
+      });
+  }, []);
+
   return (
     <React.Fragment>
       <nav>
@@ -19,11 +31,30 @@ const App: FC = () => {
           <li>
             <Link to='/login'>Login</Link>
           </li>
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+          <li>
+            { isLoggedIn ? 'ログイン中です' : 'ログインしてません' }
+          </li>
         </ul>
       </nav>
       <Switch>
-        <Route path='/about' component={About} />
-        <Route path='/login' component={Login} />
+        <Route path='/about' component={About} exact />
+        <Route
+          exact
+          path='/login'
+          render={() => (
+            <Login setIsLoggedIn={setIsLoggedIn}/>
+          )}
+        />
+        <Route
+          exact
+          path='/signup'
+          render={() => (
+            <SignUp setIsLoggedIn={setIsLoggedIn}/>
+          )}
+        />
         <Route path='/' component={Home} />
         <Redirect to='/' />
       </Switch>
