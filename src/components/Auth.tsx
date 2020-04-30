d74f43de8fec4e547549f85c0516a7af9cc2e913
sho endo <endo.sho@moneyforward.co.jp>
2020-04-30 18:51:44 +0900

    Modify directory structure

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Redirect } from 'react-router-dom';
-import { SnackbarSeverity } from './App';
+import { SnackbarSeverity } from '../App';
 
 type Props = {
   isLoggedIn: null | boolean;
