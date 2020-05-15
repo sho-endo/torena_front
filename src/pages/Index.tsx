49769c24a722f28a33daafaa8d7cd158e3a9d1b6
sho endo <endo.sho@moneyforward.co.jp>
2020-05-15 17:54:52 +0900

    Implement display menu list function

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,8 +7,14 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+
+import Loading from '../components/Loading';
 import { apiClient } from '../lib/axios';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -24,7 +30,16 @@ const useStyles = makeStyles((theme: Theme) =>
       maxHeight: 300,
       maxWidth: 400,
       margin: 'auto',
+      marginBottom: theme.spacing(10),
       overflow: 'auto',
+    },
+    formControl: {
+      margin: 'auto',
+      marginTop: theme.spacing(2),
+      minWidth: 120,
+    },
+    selectFormWrapper: {
+      textAlign: 'center',
     },
   })
 );
@@ -48,23 +63,22 @@ interface PartWithMenu {
 const Index: FC = () => {
   const classes = useStyles();
 
+  const [isLoading, setIsLoading] = useState(true);
   const [partWithMenus, setPartWithMenus] = useState<PartWithMenu[]>([]);
+  const [menus, setMenus] = useState<Menu[]>([]);
 
   useEffect(() => {
     apiClient
       .get('parts?include_menus=true')
       .then((res) => {
         setPartWithMenus(res.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         // TODO: エラーハンドリング
         console.log(err);
       });
   }, []);
-
-  const handleCilckDeleteIcon = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log(e.currentTarget.id);
-  };
 
   const parts: Part[] = [];
   partWithMenus.forEach((partWithMenu) => {
@@ -74,6 +88,35 @@ const Index: FC = () => {
     };
     parts.push(part);
   });
+
+  const handleCilckPartDeleteIcon = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(e.currentTarget.id);
+  };
+
+  const handleChangeSelectMenu = (
+    e: React.ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>
+  ) => {
+    const targetPart = partWithMenus.find((partWithMenu) => {
+      return partWithMenu.id === e.target.value;
+    });
+
+    if (targetPart == null) {
+      return;
+    }
+
+    setMenus(targetPart.menus);
+  };
+
+  const handleCilckMenuDeleteIcon = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(e.currentTarget.id);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <Container component="main" maxWidth="xl" className={classes.root}>
@@ -86,7 +129,44 @@ const Index: FC = () => {
           return (
             <ListItem divider={true} key={i}>
               <ListItemText primary={part.name} />
-              <ListItemSecondaryAction id={`${part.id}`} onClick={(e) => handleCilckDeleteIcon(e)}>
+              <ListItemSecondaryAction
+                id={`${part.id}`}
+                onClick={(e) => handleCilckPartDeleteIcon(e)}
+              >
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+      <Typography component="h1" variant="h4" align="center" className={classes.heading}>
+        メニュー一覧
+      </Typography>
+      <div className={classes.selectFormWrapper}>
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="menu-select-label">部位</InputLabel>
+          <Select defaultValue="" onChange={(e) => handleChangeSelectMenu(e)}>
+            {parts.map((part) => {
+              return (
+                <MenuItem value={part.id} key={part.id}>
+                  {part.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
+      <List className={classes.listRoot}>
+        {menus.map((menu, i) => {
+          return (
+            <ListItem divider={true} key={i}>
+              <ListItemText primary={menu.name} />
+              <ListItemSecondaryAction
+                id={`${menu.id}`}
+                onClick={(e) => handleCilckMenuDeleteIcon(e)}
+              >
                 <IconButton edge="end" aria-label="delete">
                   <DeleteIcon />
                 </IconButton>
